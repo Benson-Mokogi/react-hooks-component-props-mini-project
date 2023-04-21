c51a858b99a0fbf8cdbb658f468ad9858bb6bc48
Benson Mokogi <b.mokogi@gmail.com>
2023-04-21 13:35:05 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
 import React from "react";
 import blogData from "../data/blog";
 
-console.log(blogData);
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.blogName}</h1>
+    </header>
+  );
+}
+
+function About(props) {
+  const { imageSrc = "https://via.placeholder.com/215", aboutText } = props;
+
+  return (
+    <aside>
+      <img src={imageSrc} alt="blog logo" />
+      <p>{aboutText}</p>
+    </aside>
+  );
+}
+
+function ArticleList(props) {
+  const articles = props.posts.map((post) => (
+    <Article
+      key={post.id}
+      title={post.title}
+      date={post.date || "January 1, 1970"}
+      preview={post.preview}
+    />
+  ));
+
+  return <main>{articles}</main>;
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h3>{props.title}</h3>
+      <small>{props.date}</small>
+      <p>{props.preview}</p>
+    </article>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header blogName={blogData.name} />
+      <About imageSrc={blogData.image} aboutText={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
